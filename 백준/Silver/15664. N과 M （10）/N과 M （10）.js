8d93debab09aa07b081d301d0d77e1a994f8ab03
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-02-22 18:53:41 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.js
@@ -0,0 +1,46 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n, m;
+let num = [];
+let result = [];
+
+readline
+  .on("line", (line) => {
+    if (!n) {
+      [n, m] = line.split(" ").map(Number);
+    } else {
+      num = line.split(" ").map(Number);
+      readline.close();
+    }
+  })
+  .on("close", () => {
+    solution();
+    console.log(result.join("\n"));
+    process.exit();
+  });
+
+const solution = () => {
+  let arr = [];
+  let visited = Array(n).fill(false);
+  num.sort((a, b) => a - b);
+
+  const dfs = (x) => {
+    if (arr.length === m && !result.includes(arr.join(" "))) {
+      result.push(arr.join(" "));
+      return;
+    }
+    for (let i = x; i < num.length; i++) {
+      if (visited[i]) continue;
+      arr.push(num[i]);
+      visited[i] = true;
+      dfs(i);
+      arr.pop();
+      visited[i] = false;
+    }
+  };
+
+  dfs(0);
+};
