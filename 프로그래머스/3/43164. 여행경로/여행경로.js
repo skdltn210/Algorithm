02aa5d73192ce82cd35e934b9192e9b07bfbf7b6
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-03-08 17:55:02 +0900

    [level 3] Title: 여행경로, Time: 49.52 ms, Memory: 49.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,27 @@
+function solution(tickets) {
+    let visited = Array(tickets.length).fill(false)
+    let arr = []
+    const dfs = (start,path) => {
+        if(path.length === tickets.length+1) {
+            arr.push([...path])
+            return
+        }
+        for(let i=0;i<tickets.length;i++){
+            if(tickets[i][0] === start){
+                if(!visited[i]){
+                    visited[i] = true
+                    path.push(tickets[i][1])
+                    dfs(tickets[i][1],path)
+                    visited[i] = false
+                    path.pop()
+                }
+            }
+        }
+    }
+    dfs("ICN",["ICN"])
+    for(let i=0;i<arr.length;i++){
+        arr[i] = arr[i].join(" ")
+    }
+    arr.sort()
+    return arr[0].split(" ")
+}
