0c05a1c6294ea39e7a0a653fadda2e98fdd8e16e
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-03-09 23:37:08 +0900

    [level 3] Title: 등굣길, Time: 1.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -0,0 +1,26 @@
+function solution(m, n, puddles) {
+    let dp = Array(n).fill(0).map(()=>Array(m).fill(0))
+    dp[0][0]=1
+    for(let puddle of puddles){
+        dp[puddle[1]-1][puddle[0]-1]=-1
+    }
+    for(let i=1;i<n;i++){
+        if(dp[i][0]===-1) continue
+        else if(dp[i-1][0]===-1) dp[i][0]=0
+        else dp[i][0]=dp[i-1][0]
+    }
+    for(let i=1;i<m;i++){
+        if(dp[0][i]===-1) continue
+        else if(dp[0][i-1]===-1) dp[0][i]=0
+        else dp[0][i]=dp[0][i-1]
+    }
+    for(let i=1;i<n;i++){
+        for(let j=1;j<m;j++){
+            if(dp[i][j]===-1) continue
+            else if(dp[i-1][j]===-1) dp[i][j]=dp[i][j-1]
+            else if(dp[i][j-1]===-1) dp[i][j]=dp[i-1][j]
+            else dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000007
+        }
+    }
+    return dp[n-1][m-1]
+}
