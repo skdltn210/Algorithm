2db0c27563258258f24844c58422774de2c06a98
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-03-11 01:20:02 +0900

    [level 2] Title: 주식가격, Time: 36.62 ms, Memory: 44.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,11 @@
+function solution(prices) {
+    let arr = Array(prices.length-1).fill(1)
+    arr.push(0)
+    for(let i=0;i<prices.length-1;i++){
+        for(let j=i+1;j<prices.length-1;j++){
+            if(prices[i]<=prices[j]) arr[i]++
+            else break
+        }
+    }
+    return arr
+}
