f2e34fea40e2ba0d154a0c377b647aa0cbc51739
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-03-27 02:11:25 +0900

    Time: 0 ms (100%), Space: 53.9 MB (38.81%) - LeetHub

---
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) return "";
+  return str1.slice(0, gcd(str1.length, str2.length));
+};
+
+var gcd = function (a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+};
