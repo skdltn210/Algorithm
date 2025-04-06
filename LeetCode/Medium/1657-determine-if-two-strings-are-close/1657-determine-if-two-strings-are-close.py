53b2f76d07bcc689b4979d1cc7f03bc6fe266377
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-04-06 22:21:46 +0900

    Time: 235 ms (5.02%), Space: 18.1 MB (75.14%) - LeetHub

---
+++ LeetCode/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,13 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        map1={}
+        map2={}
+        for i in range(len(word1)):
+            if word1[i] in map1: map1[word1[i]]+=1
+            else: map1[word1[i]]=1
+        for i in range(len(word2)):
+            if word2[i] in map2: map2[word2[i]]+=1
+            else: map2[word2[i]]=1
+        if sorted(list(map1.keys())) != sorted(list(map2.keys())): return False
+        if sorted(list(map1.values())) != sorted(list(map2.values())): return False
+        return True
