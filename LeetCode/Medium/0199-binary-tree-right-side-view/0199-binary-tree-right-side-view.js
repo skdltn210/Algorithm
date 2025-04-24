6011b2a010f0cf752758a324a9cc77f53bd8f803
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-04-24 23:27:05 +0900

    Time: 0 ms (100%), Space: 55.7 MB (19.47%) - LeetHub

---
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if (!root) return [];
+    
+    let ans = [];
+    let queue = [root];
+    
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            
+            if (i === levelSize - 1) {
+                ans.push(node.val);
+            }
+            
+            if (node.left) {
+                queue.push(node.left);
+            }
+            
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+    
+    return ans;
+};
