724e01fbf3bb16b386db0c9a71e9e37c5f7558c4
koeunsoo <113167709+skdltn210@users.noreply.github.com>
2025-04-30 01:15:27 +0900

    Time: 0 ms (100%), Space: 53.7 MB (32.95%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.js
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.js
@@ -5,13 +5,11 @@
 var rob = function(nums) {
     let n=nums.length
     if(n==1) return nums[0]
-    if(n==2) return Math.max(nums[1],nums[0])
     dp=Array(n).fill(0)
     dp[0]=nums[0]
-    dp[1]=nums[1]
-    dp[2]=Math.max(dp[0]+nums[2],dp[1])
-    for(let i=3;i<n;i++){
-        dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i],dp[i-3]+nums[i])
+    dp[1]=Math.max(nums[0],nums[1])
+    for(let i=2;i<n;i++){
+        dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i])
     }
     return dp[n-1]
 };
